ae47880345b308e5d450cd7a4c1fb8000a68ef77
Melissa McMurtrie <melissa@melissakate.com>
2019-08-29 18:30:02 -0700

    updated docs

---
+++ docs/precache-manifest.c8ec061f5b42090f9d09e13b004b7483.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e7b32f9cf1f9a2927c49a47c5effd57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65493b8641a5bd9eb22f",
+    "url": "/static/css/main.ec6bae3b.chunk.css"
+  },
+  {
+    "revision": "14b996b1a923a2186ad9",
+    "url": "/static/js/2.7afa6554.chunk.js"
+  },
+  {
+    "revision": "65493b8641a5bd9eb22f",
+    "url": "/static/js/main.54d94a68.chunk.js"
+  },
+  {
+    "revision": "e9aa240c835c575984e8",
+    "url": "/static/js/runtime~main.00ad0785.js"
+  }
+]);
